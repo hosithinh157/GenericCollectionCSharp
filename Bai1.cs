74a25258a90e8756a9bd18aead796691eb74ce71
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 21:42:04 +0700

    Add files via upload

---
+++ Bai1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Bai1
+    {
+        public static void Run()
+        {
+            int n;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Console.Write("\nNhap so luong phan tu n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap so thu " + (i + 1) + ": ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                numbers.Add(num);
+            }
+
+            Console.WriteLine("\nDay so vua nhap:");
+
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
