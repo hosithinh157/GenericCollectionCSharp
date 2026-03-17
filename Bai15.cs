f806224d6ec97caff66058d73c282fcab556923a
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 22:05:41 +0700

    Add files via upload

---
+++ Bai15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Bai15
+    {
+        public static void Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary.Add("hello", "xin chao");
+            dictionary.Add("book", "quyen sach");
+            dictionary.Add("computer", "may tinh");
+            dictionary.Add("student", "sinh vien");
+            dictionary.Add("teacher", "giao vien");
+
+            Console.Write("\nNhap tu tieng Anh can tra: ");
+            string word = Console.ReadLine().ToLower();
+
+            if (dictionary.ContainsKey(word))
+            {
+                Console.WriteLine("Nghia tieng Viet: " + dictionary[word]);
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay tu trong tu dien!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
