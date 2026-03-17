259b658554ef06c247f01b06939629b5cf3cc29e
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 22:07:04 +0700

    Add files via upload

---
+++ Bai18.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Student
+    {
+        public string Name;
+        public double Score;
+    }
+
+    class Bai18
+    {
+        public static void Run()
+        {
+            int n;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Console.Write("\nNhap so luong sinh vien: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Student sv = new Student();
+
+                Console.WriteLine("\nNhap sinh vien thu " + (i + 1));
+
+                Console.Write("Nhap ten: ");
+                sv.Name = Console.ReadLine();
+
+                Console.Write("Nhap diem: ");
+                sv.Score = Convert.ToDouble(Console.ReadLine());
+
+                students.Add(sv);
+            }
+
+            Student maxStudent = students[0];
+
+            foreach (Student sv in students)
+            {
+                if (sv.Score > maxStudent.Score)
+                {
+                    maxStudent = sv;
+                }
+            }
+
+            Console.WriteLine("\nSinh vien co diem cao nhat:");
+            Console.WriteLine("Ten: " + maxStudent.Name);
+            Console.WriteLine("Diem: " + maxStudent.Score);
+
+            Console.ReadKey();
+        }
+    }
+}
