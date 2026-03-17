5e98c77b2134d221420ee8742d7612b2183a0712
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 21:45:19 +0700

    Add files via upload

---
+++ Bai7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Bai7
+    {
+        public static void Run()
+        {
+            int n;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Console.Write("\nNhap so luong phan tu: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap so thu " + (i + 1) + ": ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                numbers.Add(num);
+            }
+
+            numbers.Sort();
+
+            Console.WriteLine("\nDanh sach sau khi sap xep tang dan:");
+
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\nNhan phim bat ky de tiep tuc...");
+            Console.ReadKey();
+        }
+    }
+}
