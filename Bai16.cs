f5e0bd1c9e768cbdc6666497ce9f4ae54a32de8a
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 22:06:00 +0700

    Add files via upload

---
+++ Bai16.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Bai16
+    {
+        public static void Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Console.Write("\nNhap chuoi: ");
+            string str = Console.ReadLine();
+
+            Dictionary<char, int> count = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (count.ContainsKey(c))
+                {
+                    count[c]++;
+                }
+                else
+                {
+                    count[c] = 1;
+                }
+            }
+
+            Console.WriteLine("\nSo lan xuat hien cua tung ky tu:");
+
+            foreach (KeyValuePair<char, int> item in count)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
