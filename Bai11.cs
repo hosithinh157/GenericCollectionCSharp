c65649897f85b86397a6b31e615fa22bc167fb24
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-17 21:52:28 +0700

    Add files via upload

---
+++ Bai11.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThuchanhCS
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
+
+    class Bai11
+    {
+        public static void Run()
+        {
+            int n;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+
+            Console.Write("\nNhap so luong sinh vien: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Student sv = new Student();
+
+                Console.WriteLine("\nNhap sinh vien thu " + (i + 1));
+
+                Console.Write("Nhap ID: ");
+                sv.Id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Nhap ten: ");
+                sv.Name = Console.ReadLine();
+
+                students.Add(sv);
+            }
+
+            Console.WriteLine("\nDanh sach sinh vien:");
+
+            foreach (Student sv in students)
+            {
+                Console.WriteLine("ID: " + sv.Id + " - Ten: " + sv.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
